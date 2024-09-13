6afc276beb89bd92cdf1798c0eab14a7fd4d0691
MavericCod <921chavanprem@gmail.com>
2024-09-13 20:27:30 +0530

    completed assignment

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import nodemailer from "nodemailer";
+
+const port = 6996;
+const app = express();
+app.use(cors({ options: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// console.log(process.env.my_username); // check username
+// console.log(process.env.my_password); // check password
+
+
+app.post("/test", (req, res) => console.log("hello"));
+
+app.post("/sendEmail", (req, res) => {
+    const { name, email, message } = req.body;
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.my_username,
+            pass: process.env.my_password,
+        },
+    });
+
+    let msg = {
+        from: process.env.my_username,
+        to: process.env.my_username,
+        subject: "hello",
+        text: `${name} with email ${email} has a message for you - ${message}`,
+    };
+
+    transporter
+        .sendMail(msg)
+        .then((info) => {
+            console.log("Email Sent: " + info.response);
+            res.status(200).send("Email sent successfully");
+        })
+        .catch((err) => {console.log(err);});
+});
+
+app.listen(port, () => console.log("Server has started at port = " + port));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import axios from "axios"
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [dataSaved, setDataSaved] = useState("")
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try{const response = await axios.post("http://localhost:6996/sendEmail", {
+      name, 
+      email, 
+      message 
+    });
+    if (response.status === 200 && response.statusText === "ok"){
+      setDataSaved(!dataSaved);
+    }}
+    catch(e){
+      console.log(e);
+    }
+  }
+
+
+  return (
+    <>
+    {dataSaved ? <p className="Success">Email has been sent</p> : " "}
+      <h2>Contact Form</h2>
+      <form action="" onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder='Enter your Name' 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+         />
+        <input 
+        type="text" 
+        placeholder='Enter your email' 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+         name="" 
+         value={message}
+         placeholder='Enter your Message'
+         onChange={(e) => setMessage(e.target.value)}
+         ></textarea>
+
+         <button type="submit">Send Email</button>
+      </form>
+    </>
+  )
+}
+
+export default App
